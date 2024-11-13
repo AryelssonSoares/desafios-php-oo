0fd816891d04002ae9e302c5dfdd33b2f66357e4
AryelssonSoares <aryelssonsoares06@gmail.com>
2024-11-13 18:44:04 -0300

    Conclusão do desafio 2

--- desafio2/index.php
+++ desafio2/index.php
@@ -1,9 +1,44 @@
 <?php
     Class Veiculo {
-        public $marca;
-        public $modelo;
+        protected $marca;
+        protected $modelo;
 
-        public function mover() {
-            echo "Veiculo se movendo";
+        public function __construct($marca, $modelo)
+        {
+            $this->marca = $marca;
+            $this->modelo = $modelo;
         }
+
+        public function __get($name)
+        {
+            return $name;
+        }
+
+        public function mover() 
+        {
+            echo $this->modelo . " se movendo";
+        }
+    }
+
+    class Carro extends Veiculo {
+        public function mover() {
+            echo "Carro " . $this->modelo . " se movendo";
+        }
+    }
+
+    class Moto extends Veiculo {
+        public function mover() {
+            echo "Moto " . $this->modelo . " se movendo";
+        }
+    }
+
+    $veiculos = [
+        new Carro("Toyota", "Corolla"),
+        new Carro("Honda", "Civic"),
+        new Moto("Yamaha", "YZF-R3"),
+        new Moto("Honda", "CB 500")
+    ];
+
+    foreach($veiculos as $veiculo) {
+        echo $veiculo->mover() . "<br>";
     }
